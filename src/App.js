4e3f48403bf125e5bfc334c5822cdc4478ca96ea
khampheeraphop <poplowplay1@gmail.com>
2024-04-24 14:00:14 +0700

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignIn from './components/Signin'
+import Home from './components/Home'
+import SignUp from './components/SignUp.Popup'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/SignIn' element={<SignIn />}></Route>
+        <Route path='/Booknow' element={<Home/>}></Route>
+        <Route path='/SignUp' element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
